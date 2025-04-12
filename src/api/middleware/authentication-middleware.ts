62477ebb146b6e891d182074ccaeb0ba5e9cd1e9
Husni <husniwfayo@gmail.com>
2025-04-12 23:30:46 +0530

    fixed auth issues

--- src/api/middleware/authentication-middleware.ts
+++ src/api/middleware/authentication-middleware.ts
@@ -1,14 +1,7 @@
 import { Request, Response, NextFunction } from "express"
 import UnauthorizedError from "../../domain/errors/unauthorized-error";
 
-
-interface AuthenticatedRequest extends Request {
-    auth?: {
-        userId: string;
-    };
-}
-
-export const isAuthenticated = (req: AuthenticatedRequest, res: Response, next: NextFunction) => {
+export const isAuthenticated = (req: Request, res: Response, next: NextFunction) => {
     if (!req.auth?.userId) {
         throw new UnauthorizedError("Unauthorized");
     }
